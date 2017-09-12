16824abdf833a3eed5b7bb167a3cdde2ace2de52
Andreas Schmittner <aschmitt@coas.oregonstate.edu>
2017-09-11 17:32:58 -0700

    added comment

--- config_UVic_env.sh
+++ config_UVic_env.sh
@@ -2,6 +2,8 @@
 # A farily dumb script to generate the correct mobi1.9nb.q file. This does no
 # error checking and will blow away whatever mobi1.9nb.q file you already
 # have to BE CAREFUL!
+# Use this script to submit a job witn qsub filename. Change your filename to
+# reflect your current run. This way you'll be able to distinguish runs with qstat.
 
 FILENAME="mobi1.9nb.q"
 WORKING_DIR=$(dirname -- $(readlink -fn -- "$0"))
